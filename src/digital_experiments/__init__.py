98dd0eb480338cfb641c0d381bda8dadf5c05a4e
John Gardner <gardner.john97+dphil@gmail.com>
2023-01-20 14:48:31 +0000

    Bump version 0.1.5 -> 0.1.6

--- src/digital_experiments/__init__.py
+++ src/digital_experiments/__init__.py
@@ -1,4 +1,4 @@
 from digital_experiments.core import current_directory, experiment
 from digital_experiments.querying import experiments_for, get_artefacts, to_dataframe
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
