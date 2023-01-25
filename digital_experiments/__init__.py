ec447df91ac0915104b16d7f49959b622a0d7452
John Gardner <gardner.john97+dphil@gmail.com>
2023-01-25 16:09:28 +0000

    restructure package

--- digital_experiments/__init__.py
+++ digital_experiments/__init__.py
@@ -2,8 +2,8 @@ from digital_experiments.core import current_directory, dont_record, experiment
 from digital_experiments.querying import (
     experiments_for,
     get_artefacts,
+    results_for,
     to_dataframe,
-    to_df,
 )
 
 __version__ = "0.1.9"
--- experiments/modified_himmelbau/code.py
+++
@@ -1,4 +0,0 @@
-@experiment(backend="csv")
-def modified_himmelbau(x, y):
-    himmelbau = (x ** 2 + y - 11) ** 2 + (x + y ** 2 - 7) ** 2
-    return himmelbau + 0.5 * x + y + 20
