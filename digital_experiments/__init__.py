7e0c3e63f295297644ccdb8ce70931244cecd820
John Gardner <gardner.john97+dphil@gmail.com>
2023-02-22 20:07:20 +0000

    clean up old source code

--- digital_experiments/__init__.py
+++
@@ -1,9 +0,0 @@
-from digital_experiments.core import current_directory, dont_record, experiment
-from digital_experiments.querying import (
-    experiments_for,
-    get_artefacts,
-    results_for,
-    to_dataframe,
-)
-
-__version__ = "0.1.9"
--- digital_experiments/random.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-import numpy as np
-
-repeatable_random = np.random.RandomState(seed=1)
-truly_random = np.random.RandomState(seed=datetime.now().microsecond)
