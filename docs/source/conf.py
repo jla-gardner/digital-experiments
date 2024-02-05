0f9835bd7eeaaf5e5f5ea1e5c0d5227a83300d39
John Gardner <gardner.john97+dphil@gmail.com>
2024-02-05 11:35:49 +0000

    Bump version 2.0.2 -> 2.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "digital-experiments"
 copyright = "2023, John Gardner"
 author = "John Gardner"
-release = "2.0.2"
+release = "2.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
